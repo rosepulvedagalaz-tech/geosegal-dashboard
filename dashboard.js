fe97f3f113a4588fca078acffd4c26e10fb67304
rosepulvedagalaz-tech <rosepulveda.galaz@gmail.com>
2026-03-10 17:49:20 -0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,29 @@
+fetch("data/data.json")
+.then(response => response.json())
+.then(data => {
+
+document.getElementById("disponibilidad").textContent =
+data.kpis.disponibilidad + "%";
+
+document.getElementById("cumplimiento").textContent =
+data.kpis.cumplimiento + "%";
+
+document.getElementById("revision").textContent =
+data.kpis.revision + "%";
+
+
+const ctx = document.getElementById('fallasChart');
+
+new Chart(ctx,{
+type:'line',
+data:{
+labels:data.meses,
+datasets:[{
+label:'Fallas por mes',
+data:data.fallas
+}]
+}
+});
+
+});
 
