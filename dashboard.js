4b91b508f7f6c9cceb2a8255546e781e2c061f5f
rosepulvedagalaz-tech <rosepulveda.galaz@gmail.com>
2026-03-10 18:25:46 -0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -11,6 +11,11 @@ data.kpis.cumplimiento + "%";
 document.getElementById("revision").textContent =
 data.kpis.revision + "%";
 
+document.getElementById("flotaTotal").textContent = data.flota.total;
+document.getElementById("flotaOperativa").textContent = data.flota.operativos;
+document.getElementById("flotaMantencion").textContent = data.flota.mantencion;
+document.getElementById("flotaFuera").textContent = data.flota.fuera_servicio;
+
 
 const ctx = document.getElementById('fallasChart');
 
@@ -26,4 +31,5 @@ data:data.fallas
 });
 
 });
+
 
