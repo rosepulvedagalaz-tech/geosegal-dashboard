96d45f96eed8f24ee926178ccf2428e8d4a9d9db
Rodrigo Sepulveda <rosepulveda.galaz@gmail.com>
2026-03-11 13:01:48 -0300

    Agrego alertas de vencimiento en dashboard para Demo

--- buses.js
+++ buses.js
@@ -1,3 +1,41 @@
+async function cargarBuses(){
+
+const response = await fetch("data/buses.json")
+const data = await response.json()
+
+const tabla = document.querySelector("#tablaBuses tbody")
+
+data.buses.forEach(bus => {
+
+tabla.innerHTML += `
+
+<tr>
+
+<td>${bus.patente}</td>
+<td>${bus.marca}</td>
+<td>${bus.modelo}</td>
+<td>${bus.anio}</td>
+
+<td>
+<button onclick="verBus('${bus.patente}')">
+Ver ficha
+</button>
+</td>
+
+</tr>
+
+`
+
+})
+
+}
+
+function verBus(patente){
+
+window.location.href = "bus.html?patente=" + patente
+
+}
+
 async function buscarBus(){
 
 const patente = document.getElementById("buscador").value.toUpperCase()
@@ -18,3 +56,5 @@ document.getElementById("resultado").innerHTML = "Bus no encontrado"
 }
 
 }
+
+cargarBuses()
